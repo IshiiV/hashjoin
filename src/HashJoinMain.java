b14bc1e0162f63cdbbcdaad147e95f9194493313
IshiiV <ishii.v.4@gmail.com>
2018-06-18 08:17:28 -0300

    Update HashJoinMain.java

--- src/HashJoinMain.java
+++ src/HashJoinMain.java
@@ -3,11 +3,13 @@
 public class HashJoinMain {
 
 	public static void main(String[] args) {
-		HashJoinHeader Hjh1 = new HashJoinHeader();
+		long tempoInicial = System.currentTimeMillis();
 		
+		HashJoinHeader Hjh1 = new HashJoinHeader();
 		
 		Hjh1.cs1.run();
 		Hjh1.cs2.run();
 		Hjh1.HashJoinFunction();
+		System.out.println("Tarefe executada em " + (System.currentTimeMillis() - tempoInicial)+" milissegundos.");
 }
 }
