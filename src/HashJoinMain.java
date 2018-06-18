b3e837d076967ff297fef5779403f1262b5a35d7
IshiiV <ishii.v.4@gmail.com>
2018-06-18 06:34:03 -0300

    Update HashJoinMain.java

--- src/HashJoinMain.java
+++ src/HashJoinMain.java
@@ -3,11 +3,11 @@
 public class HashJoinMain {
 
 	public static void main(String[] args) {
-		String tabela1 = "nation.csv";
-		String tabela2 = "supplier.csv";
-	
-		HashJoin hj = new HashJoin(tabela1, tabela2);
+		HashJoinHeader Hjh1 = new HashJoinHeader();
 		
-		hj.run();
+		
+		Hjh1.cs1.run();
+		Hjh1.cs2.run();
+		Hjh1.HashJoinFunction();
 }
 }
