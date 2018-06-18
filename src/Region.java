2d4aab19cc954dd26bfe2faf829d46f0e4fcf9a8
IshiiV <ishii.v.4@gmail.com>
2018-06-18 06:32:59 -0300

    Delete Region.java

--- src/Region.java
+++
@@ -1,9 +0,0 @@
-
-public class Region {       
-	String id, name,comment;   
-	public Region(String id, String name, String comment) {    
-	    this.id = id;    
-	    this.name = name;    
-	    this.comment = comment;  
-	}    
-} 
